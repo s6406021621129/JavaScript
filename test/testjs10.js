6c20d6591c9372700817a75449bfc15dae86eccd
thanakorn <s6406021621129@email.kmutnb.ac.th>
2023-01-26 10:43:50 +0700

    testjs

---
+++ test/testjs10.js
@@ -0,0 +1,7 @@
+//create a function that takes a string(ipv4 address in standard dot decimal format)and returns true if the ip is valid or false if it's not.for information on ipv4 formatting please refer to the resources in the resources tab.
+//isvalidip(*1.2.3.4*) => ture
+//isvalidip(*1.2.3*) => false
+//isvalidip(*1.2.3.4.5*) => false
+//isvalidip(*223.45.67.89*) =>ture
+//isvalidip(*123.456.78.90*) => false
+//isvalidip(*123.045.067.089*) => false
---
+++ test/testjs9.js
@@ -0,0 +1,7 @@
+//write a function that takes the number of second and returns the digital format clock time as a string.time should be counted from 00:00:00
+//digitalclock(5025) => "01:23:45"
+//5025 second is 1 hour,23 min,45 secs.
+//digitalclock(61201) => "17:00:01"
+// NO AM/PM. 24h format.
+//digitalclock(87000) => "00:10:00"
+//It's 00:10 next day.
